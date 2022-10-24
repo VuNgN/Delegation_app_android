d8cf736b4b2fb238a86893d2060564cb219c6add
vu.nguyengoc <vu.nguyengoc1@vti.com.vn>
2022-10-24 16:12:22 +0700

    Implement: BlockUninstallFragment.kt

--- app/src/main/java/com/vti/android/delegatedscopemanagement/testapp/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/vti/android/delegatedscopemanagement/testapp/ui/main/activity/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.vti.android.delegatedscopemanagement.testapp.ui.main.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
-import com.vti.android.delegatedscopemanagement.testapp.R
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.vti.android.delegatedscopemanagement.testapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
